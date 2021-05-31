995354221d4924f8284a1fcf984fc2566781c632
三浦政大 <masao.miura@i-vinci.co.jp>
2021-05-31 20:19:51 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+const ejs = require('ejs');
+
+const indexHtml = fs.readFileSync('./index.html', 'utf8');
+const createUserEjs = fs.readFileSync('./create-user.ejs', 'utf8');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(indexHtml);
+    res.end();
+    return;
+  }
+
+  if (req.url === '/create-user' && req.method === 'POST') {
+    let data = '';
+    req
+      .on('data', (chunk) => {
+        data += chunk;
+      })
+      .on('end', () => {
+        const parseData = qs.parse(data);
+        const content = ejs.render(createUserEjs, {
+          name: parseData.name,
+          age: parseData.age,
+        });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(content);
+        res.end();
+      });
+    return;
+  }
+
+  res.writeHead(302, { Location: '/' });
+  res.end();
+});
+server.listen(3000);
+
+const redirectIndex = (req, res) => {};
